8abfecec4afddbd04847d67f19e4813316b5001d
CrazybutSolid <wachichornia@gmail.com>
2022-12-22 14:35:14 +0100

    Initial Commit

---
+++ Ask Tais/Ask_TaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ask_TaisApp.swift
+//  Ask Tais
+//
+//  Created by Antonio Montani on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ask_TaisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
